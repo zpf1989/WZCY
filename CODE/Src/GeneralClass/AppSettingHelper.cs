c09a2d0ff2c084c5cf0612c345ab18cbfa29efde
zpf1989 <751682472@qq.com>
2016-06-14 16:23:21 +0800

    添加应用程序配置辅助类

---
+++ CODE/Src/GeneralClass/AppSettingHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace OA.GeneralClass
+{
+    public class AppSettingHelper
+    {
+        /// <summary>
+        /// 获取AppSettings设置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Get(string key)
+        {
+            string value = ConfigurationManager.AppSettings[key].ToString();
+            return value;
+        }
+    }
+}
